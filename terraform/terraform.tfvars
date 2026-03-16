32f7302df258dd3edc10424a043241bafd2e84ed
zafar-m3n <thezafar.m3n@gmail.com>
2026-03-16 17:14:53 +0530

    Terraform

---
+++ terraform/terraform.tfvars
@@ -0,0 +1,14 @@
+subscription_id         = "cc424e21-ce14-46a5-9481-4e8770330300"
+resource_group_name     = "jokes-assignment-rg"
+resource_group_location = "centralindia"
+
+admin_username      = "azureuser"
+ssh_public_key_path = "C:/Users/ZAFARULLAH/.ssh/id_rsa.pub"
+kong_dns_label      = "zulq-jokes-kong"
+
+indonesia_location = "indonesiacentral"
+india_location     = "centralindia"
+malaysia_location  = "malaysiawest"
+
+kong_vm_size    = "Standard_B2pts_v2"
+default_vm_size = "Standard_B1s"
---
+++ terraform/variables.tf
@@ -0,0 +1,54 @@
+variable "subscription_id" {
+  type        = string
+  description = "Azure subscription ID"
+}
+
+variable "resource_group_name" {
+  type        = string
+  description = "Resource group name"
+}
+
+variable "resource_group_location" {
+  type        = string
+  description = "Resource group location"
+}
+
+variable "admin_username" {
+  type        = string
+  description = "Admin username for all VMs"
+}
+
+variable "ssh_public_key_path" {
+  type        = string
+  description = "Path to SSH public key"
+}
+
+variable "kong_dns_label" {
+  type        = string
+  description = "DNS label for Kong public IP"
+}
+
+variable "indonesia_location" {
+  type        = string
+  description = "Azure region for Indonesia resources"
+}
+
+variable "india_location" {
+  type        = string
+  description = "Azure region for India resources"
+}
+
+variable "malaysia_location" {
+  type        = string
+  description = "Azure region for Malaysia resources"
+}
+
+variable "kong_vm_size" {
+  type        = string
+  description = "VM size for Kong VM"
+}
+
+variable "default_vm_size" {
+  type        = string
+  description = "Default VM size for non-Kong VMs"
+}
